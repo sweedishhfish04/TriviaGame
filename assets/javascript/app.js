95f4b52aa595b185cd9fe04d260a3cb84be5c28f
sweedishhfish04 <godziwa4@yahoo.com>
2018-06-11 18:11:55 -0500

    questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,92 @@
+var questions = [
+    {
+        question: "The Vikings finished with a total record of 16-6 (including Pre/post Season)",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+    {
+        question: "The Vikings won all of their division games",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "False"
+    },
+
+    {
+        question: "Adrian Peterson got 103 Yards against the Vikings in Game 1",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "False"
+    },
+
+    {
+        question: "The Vikings out scored the Packers by 29 Points",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+    {
+        question: "The Vikings had the 2nd best overall team defense",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "False"
+    },
+
+    {
+        question: "Adam Thielen had four games with 100+ yards receiving",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+    {
+        question: "The Vikings had 3 players tied with a team high of 8 TDs",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+    {
+        question: "The Vikings gave up five touchdowns off turn overs",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "False"
+    },
+
+    {
+        question: "Case Keenum forced 2 fumbles",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+    {
+        question: "Diggs made a last second catch to win the second round of the playoffs",
+        answers: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswers: "True"
+    },
+
+]
